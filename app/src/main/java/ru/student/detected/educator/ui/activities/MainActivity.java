4a4fa1c1733792e598749c7ba86565a4ba862945
Arthur Deverson <arturosardaryan@yandex.ru>
2023-04-16 02:31:41 +0300

    Finish SplashScreen

--- app/src/main/java/ru/student/detected/educator/ui/activities/MainActivity.java
+++ app/src/main/java/ru/student/detected/educator/ui/activities/MainActivity.java
@@ -1,6 +1,8 @@
 
 package ru.student.detected.educator.ui.activities;
 
+import static androidx.core.splashscreen.SplashScreen.*;
+
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,14 +10,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import ru.student.detected.page1.R;
 
 public class MainActivity extends AppCompatActivity {
-
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		installSplashScreen(this);
 		setContentView(R.layout.main);
 	}
-
-
 }
 	
 	
